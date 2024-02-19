cf5266315a3a171070bee6f648f4ee5cc774fc47
peixed351 <peixed351@gmail.com>
2024-02-19 18:21:12 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -39,7 +39,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://github.com/metu-kovan/METU-ALET"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 14050002
+PREVIEW_IMAGE_ID: int = 14085687
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/alet"
