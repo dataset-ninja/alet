f9a97bb8a3011a2031f490678d5c186a94bc2d91
peixed351 <peixed351@gmail.com>
2024-02-19 18:37:56 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "METU-ALET"
 PROJECT_NAME_FULL: str = "METU-ALET: A Dataset for Tool Detection in the Wild"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
